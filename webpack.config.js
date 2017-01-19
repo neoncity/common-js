0601d66ecc098d0c254a3b2911399a270ee116f7
Horia Coman <horia141@gmail.com>
2017-01-19 20:02:02 +0200

    Add code and compilation for it.

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const failPlugin = require('webpack-fail-plugin');
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    target: 'node',
+    entry: {
+        lib: './src/index.ts',
+        tests: './tests/index.ts',
+    },
+    output: {
+        path: path.resolve(__dirname, 'out'),
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [{
+            test: /\.ts$/,
+            include: [
+                path.resolve(__dirname, 'src'),
+                path.resolve(__dirname, 'tests')
+            ],
+            loader: 'ts',
+            query: {
+                configFileName: 'tsconfig.json',
+                silent: true
+            }
+        }]
+    },
+    resolve: {
+        extensions: ['', '.js', '.ts'],
+        root: [
+            path.resolve(__dirname, 'src'),
+            path.resolve(__dirname, 'tests')
+        ]
+    },
+    plugins: [
+        failPlugin,
+        new webpack.IgnorePlugin(/LICENSE/), // From moch
+        new webpack.IgnorePlugin(/template.html/), // From mocha        
+    ],
+    devtool: 'source-map'
+}
