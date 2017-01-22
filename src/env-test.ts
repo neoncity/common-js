d218e9347c308e2b89e94c8d18528a8b0124c8c3
Horia Coman <horia141@gmail.com>
2017-01-22 20:32:06 +0200

    Properly require env.

--- src/env-test.ts
+++ src/env-test.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai'
 import 'mocha'
 
-import { Env, parseEnv, isLocal} from '../src/env'
+import { Env, parseEnv, isLocal} from './env'
 
 describe('Env', () => {
     describe('parseEnv', () => {
