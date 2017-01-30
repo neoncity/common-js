0f3c9fb006312aa6a47c2b6caa110943b1dea001
Horia Coman <horia141@gmail.com>
2017-01-30 10:14:04 +0200

    Use new hierarchy for the array extractors as well.

--- src/marshall/index.ts
+++ src/marshall/index.ts
@@ -148,35 +148,58 @@ export class OptionalMarshaller<T> implements Marshaller<T|null> {
 }
 
 
-export class ArrayMarshaller<T extends Object> implements Marshaller<T[]> {
-    private _inner: Marshaller<T>;
-
-    constructor(inner: Marshaller<T>) {
-	this._inner = inner;
-    }
-
-    extract(raw: any): T[] {
+export abstract class BaseArrayMarshaller<T> extends RaiseBuildFilterMarshaller<any[], T[]> {
+    raise(raw: any): any[] {
 	if (!Array.isArray(raw)) {
 	    throw new ExtractError('Non-array input');
 	}
 
+	return raw;
+    }
+
+    lower(a: any[]): any {
+	return a;
+    }
+}
+
+
+export class ArrayMarshaller extends BaseArrayMarshaller<any> {
+    build(a: any[]) {
+	return a;
+    }
+
+    unbuild(b: any[]) {
+	return b;
+    }
+}
+
+
+export class SingleArrayMarshaller<T> extends BaseArrayMarshaller<T> {
+    private _inner: Marshaller<T>;
+
+    constructor(inner: Marshaller<T>) {
+	super();
+	this._inner = inner;
+    }
+
+    build(a: any[]): T[] {
 	const cooked: T[] = [];
 
-	for (let elem of raw) {
+	for (let elem of a) {
 	    cooked.push(this._inner.extract(elem));
 	}
 
 	return cooked;
     }
 
-    pack(cooked: T[]): any {
-	const raw: any[] = [];
+    unbuild(cooked: T[]): any[] {
+	const a: any[] = [];
 
 	for (let elem of cooked) {
-	    raw.push(this._inner.pack(elem));
+	    a.push(this._inner.pack(elem));
 	}
 
-	return raw;
+	return a;
     }
 }
 
