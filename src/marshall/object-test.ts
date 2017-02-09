691fc6de6fee9214f3979fd1e167019d2f7f939c
Horia Coman <horia141@gmail.com>
2017-02-09 18:38:51 +0200

    Even more object testing.

--- src/marshall/object-test.ts
+++ src/marshall/object-test.ts
@@ -1,8 +1,11 @@
 import { expect } from 'chai'
 import 'mocha'
 
-import { NumberMarshaller } from './number'
+import { IdMarshaller } from './id'
+import { IntegerMarshaller, NumberMarshaller } from './number'
 import { ObjectMarshaller, UntypedObjectMarshaller, MarshalSchema } from './object'
+import { OptionalMarshaller } from './optional'
+import { StringMarshaller } from './string'
 
 
 describe('UntypedObjectMarshaller', () => {
@@ -103,28 +106,75 @@ describe('ObjectMarshaller', () => {
         }
     }
 
-    const Objects = [
-        [{x: 10, y: 20}, new Point(10, 20), 30],
-        [{x: 11, y: 22}, new Point(11, 22), 33]
-    ];
-
-    const PointsSchema: MarshalSchema<Point> = {
+    const PointSchema: MarshalSchema<Point> = {
         x: new NumberMarshaller(),
         y: new NumberMarshaller()
     };
+
+    const Points = [
+        [{x: 10, y: 20}, new Point(10, 20), 30],
+        [{x: 11, y: 22}, new Point(11, 22), 33],
+        [{x: 12, y: 24, z: 36}, new Point(12, 24), 36]
+    ];
+
+    class User {
+	id: number;
+	name: string;
+	age: number;
+
+	homePosition: Point;
+	officePosition: Point|null;
+
+	constructor(id: number, name: string, age: number, homePosition: Point, officePosition?: Point) {
+	    this.id = id;
+	    this.name = name;
+	    this.age = age;
+	    this.homePosition = homePosition;
+	    if (typeof officePosition == 'undefined') {
+		this.officePosition = null;
+	    } else {
+		this.officePosition = officePosition;
+	    }
+	}
+    }
+
+    const UserSchema: MarshalSchema<User> = {
+	id: new IdMarshaller(),
+	name: new StringMarshaller(),
+	age: new IntegerMarshaller(),
+	homePosition: new ObjectMarshaller<Point>(Point, PointSchema),
+	officePosition: new OptionalMarshaller<Point>(new ObjectMarshaller<Point>(Point, PointSchema))
+    };
+
+    const Users = [
+	[{id: 1, name: 'John', age: 21, homePosition: {x: 0, y: 20}, officePosition: {x: 10, y: 20}},
+	 new User(1, 'John', 21, new Point(0, 20), new Point(10, 20))],
+	[{id: 2, name: 'Jane', age: 22, homePosition: {x: 10, y: 30}},
+	 new User(2, 'Jane', 22, new Point(10, 30))],
+	[{id: 3, name: 'Harry', age: 24, homePosition: {x: 100, y: 300}, money: 1000},
+	 new User(3, 'Harry', 24, new Point(100, 300))],	
+    ];
     
     describe('extract', () => {
-        for (let [raw, object, coordsSum] of Objects) {
-            it(`should extract ${JSON.stringify(object)}`, () => {
-                const objectMarshaller = new ObjectMarshaller<Point>(Point, PointsSchema);
-                const extracted: Point = objectMarshaller.extract(raw);
-
-                console.log(extracted);
+        for (let [raw, point, coordsSum] of Points) {
+            it(`should extract ${JSON.stringify(raw)}`, () => {
+                const pointMarshaller = new ObjectMarshaller<Point>(Point, PointSchema);
+                const extracted: Point = pointMarshaller.extract(raw);
 
                 expect(extracted).to.be.an.instanceof(Point);
-                expect(extracted).to.eql(object);
+                expect(extracted).to.eql(point);
                 expect(extracted.coordsSum()).to.eql(coordsSum);
             });
         }
+
+	for (let [raw, user] of Users) {
+	    it(`should extract ${JSON.stringify(raw)}`, () => {
+		const userMarshaller = new ObjectMarshaller<User>(User, UserSchema);
+		const extracted: User = userMarshaller.extract(raw);
+
+                expect(extracted).to.be.an.instanceof(User);
+                expect(extracted).to.eql(user);
+	    });
+	}
     });
 });
