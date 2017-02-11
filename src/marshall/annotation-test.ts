fac0cc0dcf51a34e58d7d4de114d67a144f228d5
Horia Coman <horia141@gmail.com>
2017-02-11 22:30:32 +0200

    Tests for annotations.

--- src/marshall/annotation-test.ts
+++ src/marshall/annotation-test.ts
@@ -1,9 +1,11 @@
 import { expect } from 'chai'
 import 'mocha'
 
+import { ArrayMarshaller} from './array'
+import { MapMarshaller} from './map'
 import { NumberMarshaller } from './number'
 import { OptionalMarshaller } from './optional'
-import { OptionalOf } from './annotation'
+import { ArrayOf, MapOf, OptionalOf } from './annotation'
 
 
 describe('OptionalOf', () => {
@@ -11,5 +13,23 @@ describe('OptionalOf', () => {
         const marshaller = new (OptionalOf(NumberMarshaller));
 
         expect(marshaller).to.be.an.instanceof(OptionalMarshaller);
+    });
+});
+
+
+describe('ArrayOf', () => {
+    it('should create an ArrayMarshaller', () => {
+        const marshaller = new (ArrayOf(NumberMarshaller));
+
+        expect(marshaller).to.be.an.instanceof(ArrayMarshaller);
+    });
+});
+
+
+describe('MapOf', () => {
+    it('should create an MapMarshaller', () => {
+        const marshaller = new (MapOf(NumberMarshaller));
+
+        expect(marshaller).to.be.an.instanceof(MapMarshaller);
     });
 });
